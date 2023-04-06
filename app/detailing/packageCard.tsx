8e8dcd0ac7ecf228d46be3b1a6aad665cbe7f537
Jeffrey Cheung <jeffreycheung_@live.com>
2023-04-05 22:16:08 -0700

    style card info

--- app/detailing/packageCard.tsx
+++ app/detailing/packageCard.tsx
@@ -4,44 +4,64 @@ const PackageCard = () => {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.card}>
-        <h2 className={styles.cardHeading}>Maintenance Interior Detail</h2>
-        <ul>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-        </ul>
-        <div className={styles.bottomCard}>
-
+        <div>
+          <h2 className={styles.cardHeading}>Maintenance Interior Detail</h2>
+          <ul>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+          </ul>
+        </div>
+        <div>
           <h1 className={styles.cardHeading}>$80</h1>
           <button className={styles.bookButton}>Book Now</button>
         </div>
       </div>
+
       <div className={styles.card}>
-        <h2 className={styles.cardHeading}>Maintenance Exterior Detail</h2>
-        <ul>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-        </ul>
-        <h1 className={styles.cardHeading}>$80</h1>
-        <button className={styles.bookButton}>Book Now</button>
+        <div>
+          <h2 className={styles.cardHeading}>Maintenance Exterior Detail</h2>
+          <ul>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+          </ul>
+        </div>
+        <div>
+          <h1 className={styles.cardHeading}>$80</h1>
+          <button className={styles.bookButton}>Book Now</button>
+        </div>
       </div>
+
       <div className={styles.card}>
-        <h2 className={styles.cardHeading}>Complete Maintenance Detail</h2>
-        <ul>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-          <li>wash</li>
-        </ul>
-        <h1 className={styles.cardHeading}>$150</h1>
-        <button className={styles.bookButton}>Book Now</button>
+        <div>
+          <h2 className={styles.cardHeading}>Complete Maintenance Detail</h2>
+          <ul>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+            <li>wash</li>
+          </ul>
+        </div>
+        <div>
+          <h1 className={styles.cardHeading}>$150</h1>
+          <button className={styles.bookButton}>Book Now</button>
+        </div>
       </div>
     </div>
   )
